27e5ec0d0c1d417b6ca6abd755fbe0c3647a1e3f
Shibani Sankpal <76561269+shibanisankpal@users.noreply.github.com>
2023-07-17 23:37:58 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+from datetime import datetime, date
+
+
+def get_stock_data(symbol, start_date, end_date):
+    stock_data = yf.download(symbol, start=start_date, end=end_date)
+    return stock_data
+
+def main():
+    st.title("Finance Project with Streamlit")
+    st.write("Welcome to your finance app!")
+
+    # Get user inputs
+    symbol = st.text_input("Enter the stock symbol (e.g., AAPL):", "AAPL")
+    start_date = st.date_input("Start date:", datetime(2022, 1, 1))
+    end_date = date.today()
+
+    # Fetch and display stock data
+    stock_data = get_stock_data(symbol, start_date, end_date)
+    st.subheader(f"{symbol} Stock Prices from {start_date} to {end_date}")
+    st.line_chart(stock_data["Adj Close"])
+
+
+
+if __name__ == "__main__":
+    main()
+
